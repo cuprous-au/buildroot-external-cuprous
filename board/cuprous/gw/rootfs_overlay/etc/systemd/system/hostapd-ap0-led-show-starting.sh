39a9f556b5fb72ca2370325cec27ca6b3e7a3518
Christopher Hunt <huntchr@gmail.com>
2024-08-06 10:00:07 +1000

    Mistake fixed

--- board/cuprous/gw/rootfs_overlay/etc/systemd/system/hostapd-ap0-led-show-starting.sh
+++ board/cuprous/gw/rootfs_overlay/etc/systemd/system/hostapd-ap0-led-show-starting.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-Eco "timer" > /sys/class/leds/activity/trigger
+echo "timer" > /sys/class/leds/activity/trigger
 echo 100 > /sys/class/leds/activity/delay_on
 echo 100 > /sys/class/leds/activity/delay_off
 
