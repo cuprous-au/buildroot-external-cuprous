35386b17db58cfec001d7e58748f64bc98a28c4d
Christopher Hunt <huntchr@gmail.com>
2024-08-19 15:39:59 +1000

    Reset channel We now set/reset the channel for hostapd as part of its configuration during service startup. We also restart hostapd when wpa_supplicant goes down. This logic should guarantee that the AP channel is always set to something suitable.

--- board/cuprous/gw/rootfs_overlay/etc/systemd/system/hostapd-ap0-configure.sh
+++ board/cuprous/gw/rootfs_overlay/etc/systemd/system/hostapd-ap0-configure.sh
@@ -9,6 +9,16 @@ MAC_ID=$(cat /sys/class/net/wlan0/address | tr -d ':' | tail -c5)
 sed -i -e "s/^ssid=.*/ssid=Cuprous-${MAC_ID}/" /etc/hostapd/hostapd-ap0.conf
 sed -i -e "s/^wpa_passphrase=.*/wpa_passphrase=${MAC_ID}${MAC_ID}/" /etc/hostapd/hostapd-ap0.conf
 
+# We configure ap0 to have the same channel as wlan0 if it is active, or back
+# to a sensible default if not.
+CHANNEL=$(/sbin/iwlist wlan0 channel | grep "Current Frequency" | grep -Eo '[0-9]+')
+if [ $? -eq 0 ]; then
+    CHANNEL=$(echo "$CHANNEL" | tail -1)
+else
+    CHANNEL=1
+fi
+sed -i -e "s/^channel=.*/channel=${CHANNEL}/" /etc/hostapd/hostapd-ap0.conf
+
 # We also configure the ap0 interface here as hostapd will remove it on a restart. Thus,
 # it cannot be a udev rule. Why hostapd decides that it an external responsibility to
 # create it, but not to delete it is mysterious.
