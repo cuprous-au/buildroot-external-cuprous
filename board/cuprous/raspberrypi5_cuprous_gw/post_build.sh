553843692b062e1580e4a4c75a95b2614ca0429f
Christopher Hunt <huntchr@gmail.com>
2025-04-09 16:51:45 +1000

    RPI 5 changes The changes here bring the RPI 5 board up to a reasonable working level. I'm yet to work out the WiFi stuff, but most other things are working.

--- board/cuprous/raspberrypi5_cuprous_gw/post_build.sh
+++ board/cuprous/raspberrypi5_cuprous_gw/post_build.sh
@@ -4,3 +4,9 @@ set -e
 
 mkdir -p $TARGET_DIR/etc/systemd/system/getty.target.wants
 ln -s /usr/lib/systemd/system/serial-getty@.service $TARGET_DIR/etc/systemd/system/getty.target.wants/serial-getty@ttyGS0.service 2> /dev/null || true
+
+mkdir -p $TARGET_DIR/etc/systemd/system/first-boot-complete.target.wants
+ln -s /etc/systemd/system/resizefs.service $TARGET_DIR/etc/systemd/system/first-boot-complete.target.wants/resizefs.service 2> /dev/null || true
+
+rm -f $TARGET_DIR/etc/machine-id
+
